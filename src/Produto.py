8a5d48298c5c13f4d403521dd98000b30b81f675
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2024-12-03 22:40:09 -0300

    Criando classe Produto

---
+++ src/Produto.py
@@ -0,0 +1,7 @@
+class Produto:
+    def __init__(self, nome, preco, descricao, marca, modelo):
+        self.nome = nome
+        self.preco = preco
+        self.descricao = descricao
+        self.marca = marca
+        self.modelo = modelo
